1adf84d4e19113e5de847321f9c98fe1aec5e631
Alisher Nurmatov <118418648+AliNur17@users.noreply.github.com>
2026-04-18 16:37:59 -0700

    Update openF.c

--- openF.c
+++ openF.c
@@ -1,75 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 #include "commands.h"
-
-#define MAX_NAME 128
-#define MAX_OPEN 32
-
-typedef enum {
-    TREE_ALPHA = 0,
-    TREE_NUM   = 1,
-    TREE_SYM   = 2
-} TreeType;
-
-typedef struct FileRecord {
-    char name[MAX_NAME];
-    int inode;
-    int size;
-} FileRecord;
-
-typedef struct {
-    int used;
-    int fd;
-    char name[MAX_NAME];
-    char mode[4];
-} OpenFile;
-
-extern OpenFile openTable[MAX_OPEN];
-extern int nextFD;
-extern FileRecord *searchCentral(const char *name, TreeType *outType);
+#include "fs_state.h"
 
 void openF(char *spec, char *args)
 {
-    const char *mode;
-    FileRecord *file;
-    int i;
-
-    if (args == NULL || strlen(args) == 0) {
-        printf("usage: openF [-r|-w|-rw] <file>\n");
+    if (strcmp(spec, "-list") == 0) {
+        listClosedFiles();
         return;
     }
 
-    if (strlen(spec) == 0 || strcmp(spec, "-r") == 0)
-        mode = "r";
-    else if (strcmp(spec, "-w") == 0)
-        mode = "w";
-    else if (strcmp(spec, "-rw") == 0)
-        mode = "rw";
-    else {
-        printf("openF: invalid option '%s'\n", spec);
+    File *f = findFile(args);
+    if (!f) {
+        printf("File not found.\n");
         return;
     }
 
-    file = searchCentral(args, NULL);
-    if (file == NULL) {
-        printf("openF: cannot open '%s': No such file\n", args);
-        return;
-    }
-
-    for (i = 0; i < MAX_OPEN; i++) {
-        if (!openTable[i].used) {
-            openTable[i].used = 1;
-            openTable[i].fd = nextFD++;
-            strncpy(openTable[i].name, args, MAX_NAME - 1);
-            openTable[i].name[MAX_NAME - 1] = '\0';
-            strncpy(openTable[i].mode, mode, 3);
-            openTable[i].mode[3] = '\0';
-
-            printf("opened '%s' as fd %d (%s)\n",
-                   args, openTable[i].fd, openTable[i].mode);
+    if (strcmp(spec, "-edit") == 0) {
+        if (!f->canWrite) {
+            printf("No write permission.\n");
             return;
         }
+        f->isOpen = 1;
+        f->fd = nextFD++;
+        printf("Editing %s (fd=%d)\n", args, f->fd);
+        return;
     }
 
-    printf("openF: too many open files\n");
+    if (strcmp(spec, "-content") == 0) {
+        if (!f->canRead) {
+            printf("No read permission.\n");
+            return;
+        }
+        printf("Content of %s:\n%s\n", args, f->content);
+        return;
+    }
+
+    if (strcmp(spec, "-r") == 0) {
+        f->canRead = 1;
+        f->canWrite = 0;
+    } else if (strcmp(spec, "-w") == 0) {
+        f->canRead = 0;
+        f->canWrite = 1;
+    } else {
+        f->canRead = 1;
+        f->canWrite = 1;
+    }
+
+    f->isOpen = 1;
+    f->fd = nextFD++;
+
+    printf("Opened %s (fd=%d)\n", args, f->fd);
 }
