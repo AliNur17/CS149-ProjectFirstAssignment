325bbf499a5e4bb669494e6d52b98a6ad87e6f00
Alisher Nurmatov <118418648+AliNur17@users.noreply.github.com>
2026-04-18 16:39:19 -0700

    Update searchF.c

--- searchF.c
+++ searchF.c
@@ -1,50 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include "commands.h"
-
-#define MAX_NAME 128
-
-typedef enum {
-    TREE_ALPHA = 0,
-    TREE_NUM   = 1,
-    TREE_SYM   = 2
-} TreeType;
-
-typedef struct FileRecord {
-    char name[MAX_NAME];
-    int inode;
-    int size;
-} FileRecord;
-
-extern FileRecord *searchCentral(const char *name, TreeType *outType);
+#include "fs_state.h"
 
 void searchF(char *spec, char *args)
 {
-    TreeType type;
-    FileRecord *file;
-
-    (void)spec;
-
-    if (args == NULL || strlen(args) == 0) {
-        printf("usage: searchF <file>\n");
+    if (strcmp(spec, "-dirL") == 0) {
+        listAllFiles();
         return;
     }
 
-    file = searchCentral(args, &type);
+    File *f = findFile(args);
 
-    if (file == NULL) {
-        printf("searchF: '%s' not found\n", args);
-    } else {
-        const char *treeName;
-
-        if (type == TREE_ALPHA)
-            treeName = "Alphabetic B+ Tree";
-        else if (type == TREE_NUM)
-            treeName = "Numeric B+ Tree";
-        else
-            treeName = "Symbol B+ Tree";
-
-        printf("found '%s' [inode=%d, size=%d, index=%s]\n",
-               file->name, file->inode, file->size, treeName);
+    if (!f) {
+        printf("File not found.\n");
+        return;
     }
+
+    printf("Found: %s\nPath: %s\n", f->name, f->path);
 }
