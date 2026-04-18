0f807f04a2f534290746b388d688f4c7b4aa5dfc
Alisher Nurmatov <118418648+AliNur17@users.noreply.github.com>
2026-04-18 01:30:38 -0700

    Update createF.c

--- createF.c
+++ createF.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 #include "commands.h"
+
+#define MAX_NAME 128
+
+typedef enum {
+    TREE_ALPHA = 0,
+    TREE_NUM   = 1,
+    TREE_SYM   = 2
+} TreeType;
+
+typedef struct FileRecord {
+    char name[MAX_NAME];
+    int inode;
+    int size;
+} FileRecord;
+
+extern FileRecord *searchCentral(const char *name, TreeType *outType);
+extern int insertFileCentral(const char *name);
 
 void createF(char *spec, char *args)
 {
+    TreeType type;
+    FileRecord *file;
+
+    (void)spec;
+
+    if (args == NULL || strlen(args) == 0) {
+        printf("usage: createF <file>\n");
+        return;
+    }
+
+    file = searchCentral(args, &type);
+    if (file != NULL) {
+        printf("createF: cannot create '%s': File exists\n", args);
+        return;
+    }
+
+    if (!insertFileCentral(args)) {
+        printf("createF: failed to create '%s'\n", args);
+        return;
+    }
+
+    printf("created '%s'\n", args);
 }
