6e47327df09a2a728a097ff6ef081185124d5d58
Alisher Nurmatov <118418648+AliNur17@users.noreply.github.com>
2026-04-18 16:38:16 -0700

    Update closeF.c

--- closeF.c
+++ closeF.c
@@ -1,41 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include "commands.h"
-
-#define MAX_NAME 128
-#define MAX_OPEN 32
-
-typedef struct {
-    int used;
-    int fd;
-    char name[MAX_NAME];
-    char mode[4];
-} OpenFile;
-
-extern OpenFile openTable[MAX_OPEN];
+#include "fs_state.h"
 
 void closeF(char *spec, char *args)
 {
-    int fd;
-    int i;
-
-    (void)spec;
-
-    if (args == NULL || strlen(args) == 0) {
-        printf("usage: closeF <fd>\n");
+    if (strcmp(spec, "-list") == 0) {
+        listOpenFiles();
         return;
     }
 
-    fd = atoi(args);
+    int fd = atoi(args);
 
-    for (i = 0; i < MAX_OPEN; i++) {
-        if (openTable[i].used && openTable[i].fd == fd) {
-            openTable[i].used = 0;
-            printf("closed fd %d ('%s')\n", fd, openTable[i].name);
+    for (int i = 0; i < fileCount; i++) {
+        if (files[i].fd == fd && files[i].isOpen) {
+            files[i].isOpen = 0;
+            files[i].fd = -1;
+            printf("Closed FD %d\n", fd);
             return;
         }
     }
 
-    printf("closeF: %d: Bad file descriptor\n", fd);
+    printf("Invalid file descriptor.\n");
 }
